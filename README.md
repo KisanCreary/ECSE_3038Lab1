d3d475ff10ce96ef43ccffa4b6473b0dc67d3e0c
KisanCreary <ksancreary56@gmail.com>
2025-01-30 23:42:21 -0500

    Kisan added question 1

--- README.md
+++ README.md
@@ -3,17 +3,9 @@ Purpose: Calculates the total resistance of resistors connected in parallel. (it
 Input: List of resistances (in ohms).
 Output: Prints the effective resistance in ohms, formatted to three decimal places.
 
+
 2.potential_divider()
 Purpose: Calculates the voltage drop across each resistor in a series circuit.(it is for a lab)
 
 Input: Total voltage (in volts) and a list of resistances (in ohms).
 Output: Prints the voltage drop across each resistor (formatted to two decimal places).
-
-3.temperature_check()
-Purpose: Checks if a patient's body temperature is hypothermic, normal, or hyperthermic based on the temperature and unit (Celsius or Fahrenheit).(it is for a lab)
-Input: Temperature value and unit ("C" for Celsius, "F" for Fahrenheit).
-Output: Prints a message indicating the patient's temperature condition.
-
-
-JOKE
-How do you know that elephants like swimming? They always have their trunks on.
--- app.py
+++ app.py
@@ -12,7 +12,6 @@ def parallel(resistances):
 parallel([330, 1000, 2200])
 
 
-
 def potential_divider(voltage, resistances):
 
     total_resistance = sum(resistances) 
@@ -23,32 +22,6 @@ def potential_divider(voltage, resistances):
         
         print(f"{voltage_drop:.2f}v")
 
-
 potential_divider(9, [3000, 1000])
-
-def temperature_check(temperature, unit):
-    if unit == "C":
-        if temperature < 35:
-            print("The patient is hypothermic.")
-        elif 36.5 <= temperature <= 37.5:
-            print("The patient's temperature is normal.")
-        else:
-            print("The patient is hyperthermic.")
-    elif unit == "F":
-        if temperature < 95:
-            print("The patient is hypothermic.")
-        elif 97.7 <= temperature <= 99.5:
-            print("The patient's temperature is normal.")
-        else:
-            print("The patient is hyperthermic.")
-    else:
-        print("Invalid temperature unit. Please use 'C' for Celsius or 'F' for Fahrenheit.")
-
-# Testing with the function with the examples
-temperature_check(14, "C")  # "The patient is hypothermic"
-temperature_check(37, "C")  # "The patient's temperature is normal"
-temperature_check(37, "F")  # "The patient is hypothermic"
-
-
 
 
